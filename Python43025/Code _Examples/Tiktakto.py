e1a209fe1dd15b4c1c89493c341a5ee9b7f29105
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2022-08-16 17:47:09 +0300

    Create Tiktakto.py

---
+++ Python43025/Code _Examples/Tiktakto.py
@@ -0,0 +1,46 @@
+# print(a) == print(a, end='\n')
+
+
+
+matrix = [[0,0,0],[0,0,0],[0,0,0]]
+
+# for row in range(len(matrix)):
+#     for col in range(len(matrix[row])):
+#         matrix[row][col] 
+
+## game of tiktaktoe
+flag = True
+while flag:
+    print("welcome to tiktaktoe!")
+    #player_name = input("enter your name: ")
+
+    user_move = input("enter your move 0,0: ")
+
+    user_move = user_move.split(",")
+    row = user_move[0]
+    col = user_move[1]
+
+    row = int(row)
+    col = int(col)
+
+    if matrix[row][col] == 0:
+        matrix[row][col] = 'X'
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print(matrix[i][j], end=" ")
+        print()
+
+
+
+
+
+
+# print the matrix
+
+# for row in range(3):
+
+#     for col in range(3):
+#         print(0, end=' ')
+    
+#     print()
