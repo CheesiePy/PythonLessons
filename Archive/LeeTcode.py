2adc47c1493f9b79b3a2a5354501ba416c55a266
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2022-08-21 17:33:57 +0300

    update

---
+++ Archive/LeeTcode.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def twoSum(self, nums, target, lst=[]):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+
+print(Solution().twoSum([3, 2, 4], 6))
