c0093757c7e12fb6089ba3a0123712b5507d8c68
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2022-09-11 18:46:30 +0300

    lesson seven is up!

---
+++ Python43025/Code_Examples/Tigbor1109.py
@@ -0,0 +1,33 @@
+# name = "May   Lindenberg"
+# id_number = 313131313
+# print(name[0]) # -> M
+# seperator = ",+_()&^12341%$#@!~"
+# print(len(seperator))
+
+# for i in seperator:
+#     print("the of i: ", i)
+#     # print(name, id_number, sep=i)
+
+
+# for i in range(len(name)):
+#     if i % 2 == 0:
+#         print(name[i], end=" ")
+        
+    
+# print("Amit", sep=" ", end="\n")
+# print("Amit")
+
+# name = "Amit"
+# print(name[0])
+# print(name[-4])
+# for i in range(50,0,-1):
+#     print(i)
+
+
+#function:
+param = "Amit15"
+print(param)
+
+
+#method:
+print(param.upper())
---
+++ Python43025/Lessons/lesson_seven.py
@@ -0,0 +1,48 @@
+
+## board 3x3
+
+## two players X and O
+
+## turn 
+
+"""
+    1. create a board
+    2. create two players
+    3. get coordinates from the user
+    4. check if the coordinates are valid -> in board bounds and empty
+    5. check if the game is over
+"""
+
+
+
+def create_board(size): ## [[0,0,0],[0,0,0],[0,0,0]]
+    matrix = []
+    for row in range(size):
+        matrix.append([])
+        for col in range(size):
+            matrix[row].append(0)
+    return matrix
+
+
+def print_board(board):
+    for row in range(len(board)):
+        for col in range(len(board[row])):
+            print(board[row][col], end=" ")
+        print()
+
+    
+# def create_players():
+#     pass
+
+def get_coordinates():
+    pass
+
+def check_coordinates(size):
+    pass    
+
+# def check_game_over():
+#     pass
+
+def main(): # this is were the magic happens
+    print_board(create_board(3))
+main()    
