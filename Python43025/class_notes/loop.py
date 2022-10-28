20ac3714e7f4e71bc87f75e6254b1f3371943d46
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2022-10-28 15:05:37 +0300

    update

---
+++ Python43025/class_notes/loop.py
@@ -0,0 +1,52 @@
+
+name = "TomPiratinskiy"
+
+mylist = [1, 2, 3, "MayLindenberg", 5, 6, 7, 8, 9, 10, name, []]
+
+# for i in name:
+#     print(name.index(i), i)
+
+# x = len(name)
+# print(x)
+
+
+# number = 50
+# for i in range(len(name)): #  the range is from 0 to number-1
+#     print(i)
+
+
+
+
+
+# for i in range(len(mylist)):
+#     print(mylist[i])
+
+# for i in mylist:
+#     print(i)
+
+
+
+
+# for i in range(len(name)):
+#     print(f"the char  name[{i}] is: ",name[i])
+
+the_alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+
+# start = the_alphabet.index('j')
+# stop= the_alphabet.index('u')
+
+# for i in range(start, stop+1):
+#     print(the_alphabet[i])
+
+
+
+start = 5
+stop = 10
+step = 2
+
+
+
+for i in range(start, stop, step):
+    print(the_alphabet[i])
+
