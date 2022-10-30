e71713c730499681bab7d9ce2931e530a72f5dce
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2022-10-30 18:24:37 +0200

    update

--- Python43025/Lessons/lesson_10.py
+++ Python43025/Lessons/lesson_10.py
@@ -4,11 +4,28 @@ y = "world"
 """
 1. קבלו קלט מהמשתמש 
 2. הוסיפו את האות הראשונה של הקלט לרשימה 
-3. חיזרו על 1 ו2 חמש פעמים 
-4. הדפיסו את הרשימה
+
 """
 
-user_input = input("Enter a word: ")
+
+# my_list = []
+# for i in range(5):
+#     user_input = input("Enter a word: ")
+#     mychar = user_input[0]
+#     my_list.append(mychar)
+
+# print(my_list)
+
+# mylist = []
+# mystr = "Noam"
+# mystr2 ="Aviv"
+# mylist.append(mystr2)
+# mylist.append(mystr)
+# print(mylist)
 
 
-
+"""
+בתוך לולאת while
+1. קבלו קלט מהמשתמש
+2. הוסיפו 
+"""
