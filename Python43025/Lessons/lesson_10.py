8d0f9b704cd5f8496e3b2b5deacc37b311799d5d
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2022-11-08 16:42:28 +0200

    update

--- Python43025/Lessons/lesson_10.py
+++ Python43025/Lessons/lesson_10.py
@@ -6,15 +6,36 @@ y = "world"
 2. הוסיפו את האות הראשונה של הקלט לרשימה 
 
 """
+# for xPhone_12123 in range(5):
+#     print(xPhone_12123)
 
 
-# my_list = []
+# for jelly in range(5):
+#     print(jelly)
+
+
+
+
+
 # for i in range(5):
-#     user_input = input("Enter a word: ")
-#     mychar = user_input[0]
-#     my_list.append(mychar)
+#     print("jelly")
 
-# print(my_list)
+
+# print(i)
+
+
+
+
+
+#range(9) מחזיר תווך של מספרים מ 0 - 9 לא כולל 9   
+
+my_list = [] ## יצרנו רשימה ריקה בשם המשתנה my_list
+for i in range(5): ## לולאת for רצה חמש פעמים
+    user_input = input("Enter a word: ") # קולטים קלט מהמשתמש ושומרים אותו בשתנה user_input
+    mychar = user_input[0] # המשתנה שלנו מחזיק את האות הראשונה מהחרוזת שקיבלנו מהמשתמש!
+    my_list.append(mychar) # שם את האות הראשונה ששמנו במשתנה מיצ'אר ברשימה שהגרנו קודם
+
+print(my_list)
 
 # mylist = []
 # mystr = "Noam"
